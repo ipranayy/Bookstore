66dcc543b8de9697a495bacb9821668d2f43366a
Pranay Karthik <ipranayy@gmail.com>
2023-02-11 15:31:40 +0530

    ok

--- book_store/settings.py
+++ book_store/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-a6fi-gxg#af6$o*l7u$^a+^r-ywj+w4jip(r@vsw5o*ky8!%1b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -116,3 +116,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
